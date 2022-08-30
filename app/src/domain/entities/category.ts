964e99e08c6cba9cae6774cf47a0901d2b0bb0d8
tiagoamemiya <tiagoamemiya@gmail.com>
2022-08-30 09:13:43 -0300

    feat: ajustes gerais

---
+++ app/src/domain/entities/category.ts
@@ -0,0 +1,32 @@
+import { UniqueIdEntity } from '../shared/value-objects/unique-id-entity.vo';
+
+export type CategoryProperties = {
+  name: string
+  description?: string
+  isActive?: boolean
+  createdAt?: Date
+}
+export class Category {
+    constructor(
+    public readonly props: CategoryProperties,
+    public readonly id?: UniqueIdEntity
+    ) {
+        this.id = id || new UniqueIdEntity();
+    }
+
+    get name(): string {
+        return this.props.name;
+    }
+
+    get description(): string {
+        return this.props.description ?? null;
+    }
+
+    get isActive(): boolean {
+        return this.props.isActive ?? true;
+    }
+
+    get createdAt(): Date {
+        return this.props.createdAt ?? new Date();
+    }
+}
